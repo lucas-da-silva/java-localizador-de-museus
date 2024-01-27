70650e1cc98e148799bd6402109d2847cfaa5cf4
Lucas da Silva <lucasdasilvadev1@gmail.com>
2024-01-27 09:56:35 -0300

    tests: create tests to CollectionTypeService class

---
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -0,0 +1,88 @@
+package com.betrybe.museumfinder.solution;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.service.CollectionTypeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Testando as regras de négocio da classe CollectionTypeService")
+public class CollectionTypeServiceTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private MuseumFakeDatabase database;
+
+  @Test
+  void testOneCollectionType() {
+    String collectionType = "hist";
+    long totalCount = 387;
+    CollectionTypeCount expectedCollectionTypeCount = new CollectionTypeCount(
+        new String[]{collectionType},
+        totalCount
+    );
+    Mockito.when(database.countByCollectionType(collectionType)).thenReturn(totalCount);
+
+    CollectionTypeService service = new CollectionTypeService(database);
+    CollectionTypeCount result = service.countByCollectionTypes(collectionType);
+
+    assertArrayEquals(expectedCollectionTypeCount.collectionTypes(), result.collectionTypes());
+    assertEquals(expectedCollectionTypeCount.count(), result.count());
+    Mockito.verify(database).countByCollectionType(collectionType);
+  }
+
+  @Test
+  void testMultipleCollectionType() {
+    String typesList = "hist,imag";
+    String[] collectionType = typesList.split(",");
+    long totalCount = 492;
+    CollectionTypeCount expectedCollectionTypeCount = new CollectionTypeCount(
+        collectionType,
+        totalCount
+    );
+
+    Mockito.when(database.countByCollectionType(collectionType[0]))
+        .thenReturn(387L);
+    Mockito.when(database.countByCollectionType(collectionType[1]))
+        .thenReturn(105L);
+
+    CollectionTypeService service = new CollectionTypeService(database);
+    CollectionTypeCount result = service.countByCollectionTypes(typesList);
+
+    assertArrayEquals(expectedCollectionTypeCount.collectionTypes(), result.collectionTypes());
+    assertEquals(expectedCollectionTypeCount.count(), result.count());
+    Mockito.verify(database).countByCollectionType(collectionType[0]);
+    Mockito.verify(database).countByCollectionType(collectionType[1]);
+  }
+
+  @Test
+  void testNotFoundCollectionType() {
+    String collectionType = "a";
+    long totalCount = 0;
+    CollectionTypeCount expectedCollectionTypeCount = new CollectionTypeCount(
+        new String[]{collectionType},
+        totalCount
+    );
+    Mockito.when(database.countByCollectionType(collectionType)).thenReturn(totalCount);
+
+    CollectionTypeService service = new CollectionTypeService(database);
+    CollectionTypeCount result = service.countByCollectionTypes(collectionType);
+
+    assertArrayEquals(expectedCollectionTypeCount.collectionTypes(), result.collectionTypes());
+    assertEquals(expectedCollectionTypeCount.count(), result.count());
+    Mockito.verify(database).countByCollectionType(collectionType);
+  }
+}
